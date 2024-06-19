f09a2ad9c9800f2a3c6bacf4e10e39f322a3ef38
JalenLv <sejalenlv@mail.scut.edu.cn>
2024-06-19 14:19:20 +0800

    Linux side commit

--- lua/plugins/helpers.lua
+++ lua/plugins/helpers.lua
@@ -76,11 +76,28 @@ return {
 		version = false,
 		config = function()
 			require("mini.ai").setup()
-			require("mini.pairs").setup()
 			require("mini.operators").setup()
 			require("mini.splitjoin").setup()
 			require("mini.tabline").setup()
 			require("mini.bracketed").setup()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				disable_filetype = {
+					"TelescopePrompt",
+					"spectre_panel",
+					"tex", -- disable autopair for latex files
+				},
+			})
+
+			-- If you want insert `(` after select function or method item
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
 	{
