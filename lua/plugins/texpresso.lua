aa98d8a7df84a51c80ae51d63f50d8fa74709834
Jalen Lv <jalenlv543@gmail.com>
2026-03-07 16:14:44 -0600

    remove hardcoded texpresso binary path from configuration

--- lua/plugins/texpresso.lua
+++ lua/plugins/texpresso.lua
@@ -2,9 +2,6 @@ return {
 	"let-def/texpresso.vim",
 	ft = { "tex" },
 	config = function()
-		-- Set the path to the texpresso binary
-		require("texpresso").texpresso_path = "/opt/texpresso/build/texpresso"
-
 		-- Keybindings
 		vim.keymap.set("n", "<leader>lp", "<cmd>TeXpresso %<CR>", { desc = "Run [T]ex[P]resso in root dir" })
 	end,
