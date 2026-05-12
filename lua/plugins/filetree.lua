67c68d8e1e4f4b3d43055ee5b472d09464b6271d
JalenLv <jalenlv543@gmail.com>
2026-05-12 10:33:50 -0500

    New plugin: oil-git.nvim

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,12 +1,12 @@
 function _G.get_oil_winbar()
-  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-  local dir = require("oil").get_current_dir(bufnr)
-  if dir then
-    return vim.fn.fnamemodify(dir, ":~")
-  else
-    -- If there is no current directory (e.g. over ssh), just show the buffer name
-    return vim.api.nvim_buf_get_name(0)
-  end
+	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+	local dir = require("oil").get_current_dir(bufnr)
+	if dir then
+		return vim.fn.fnamemodify(dir, ":~")
+	else
+		-- If there is no current directory (e.g. over ssh), just show the buffer name
+		return vim.api.nvim_buf_get_name(0)
+	end
 end
 
 return {
@@ -45,48 +45,13 @@ return {
 			vim.keymap.set("n", "T", oil_toggle, { desc = "[T]oggle Oil" })
 		end,
 	},
-	-- {
-	--   "nvim-tree/nvim-tree.lua",
-	--   version = "*",
-	--   lazy = false,
-	--   dependencies = {
-	--     "nvim-tree/nvim-web-devicons",
-	--   },
-	--
-	--   init = function()
-	--     -- disable netrw
-	--     vim.g.loaded_netrw = 1
-	--     vim.g.loaded_netrwPlugin = 1
-	--     -- optionally enable 24-bit colour
-	--     vim.opt.termguicolors = true
-	--   end,
-	--
-	--   config = function()
-	--     -- global mappings
-	--     -- vim.keymap.set("n", "<leader>Tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]ree [T]oggle" })
-	--     vim.keymap.set("n", "T", "<cmd>NvimTreeToggle<CR>", { desc = "[T]ree [T]oggle" })
-	--
-	--     -- custom on_attach function
-	--     -- on_attach means that this function will be called when the nvim-tree is opened
-	--     local function on_attach(bufnr)
-	--       local api = require("nvim-tree.api")
-	--
-	--       local function opts(desc)
-	--         return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	--       end
-	--
-	--       -- default mappings
-	--       api.config.mappings.default_on_attach(bufnr)
-	--
-	--       -- custom mappings
-	--       vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
-	--       vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-	--     end
-	--
-	--     -- pass to setup along with your other options
-	--     require("nvim-tree").setup({
-	--       on_attach = on_attach,
-	--     })
-	--   end,
-	-- },
+	{
+		"malewicz1337/oil-git.nvim",
+		opts = {
+			symbol_position = "signcolumn",
+			can_use_signcolumn = function()
+				return "yes"
+			end,
+		},
+	},
 }
